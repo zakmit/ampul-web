5dd968faf28cb45d811ad28ba4e56b7a7240ac63
Kai-Chih Ho <alcrolex7@gmail.com>
2026-08-30 22:01:36 +0800

    chore: update to Next.js 16

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 import createNextIntlPlugin from 'next-intl/plugin';
 
 const nextConfig: NextConfig = {
-  /* config options here */
   experimental: {
     serverActions: {
       bodySizeLimit: '10mb',
